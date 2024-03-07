8bbae2b80440c91bc50ed9e1cfbba0aad196b346
Bianca Botezatu <115445914+biancabotezatu2909@users.noreply.github.com>
2024-03-07 21:56:57 +0200

    Add files via upload

---
+++ A6/main.cpp
@@ -0,0 +1,16 @@
+#include "Tests.h"
+#include "UI.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+//    Tests tests;
+//    tests.run_all_tests();
+
+    UI ui;
+    ui.run();
+    return 0;
+}
